91892dd8eccb9c70a9975327e82c210dd10147f8
Gustavo <gustavoquintero9105@gmail.com>
2024-09-04 00:20:32 -0500

    Updated on 09-04-2024 at 00:20

--- sources/plot_andreani_scaled.py
+++ sources/plot_andreani_scaled.py
@@ -38,20 +38,20 @@ def plot_fit(n):
         outliers[1,i] = df_data[1].values[df_outliers[0].values[i+1]-1]
         # print(outliers[1][i])
 
-    plt.plot(df_data[0].values,df_data[1].values,"ko",ms=0.5)
+    plt.plot(df_data[0].values,df_data[1].values,"ko",ms=0.1)
+    plt.plot(outliers[0],outliers[1],'ro',mfc='none',ms=1,mew=0.1)
     plt.plot(t,models.andreani(t,*df_sol.values[0]),lw=1)
-    plt.plot(outliers[0],outliers[1],'ro',mfc='none',ms=3,mew=0.5)
     plt.tick_params(axis='both',direction='in')
     plt.xticks(np.arange(-1,3.1,1))
     plt.yticks(np.arange(-6,12.1,6))
-    plt.ylim(-6.5,12)
+    plt.ylim(-6.5,13)
     plt.xlim(-1.1,3.6)
     plt.savefig(parent+"/images/andreani_fitting"+str(n)+".pdf",bbox_inches = "tight")
     plt.close()
 
 def plot_log(n):
 
-    fig,ax = plt.subplots(1, 1)
+    # fig,ax = plt.subplots(1, 1)
     if n == 100: 
         file = "andreani_scaled_log_100.txt"
     elif n == 1000:
@@ -64,29 +64,49 @@ def plot_log(n):
         file = "andreani_scaled_log_1000000.txt"
 
     df_data = pd.read_table(file,delimiter=" ",header=None,skipinitialspace=True)
-    ax.tick_params(axis='both',direction='in',which='both')
-    ax.loglog(df_data[1].values,df_data[2].values,"-o",color="darkgreen",lw=0.5,ms=1)
-    ax.set_xscale('linear')
+    # ax.tick_params(axis='both',direction='in',which='both')
+    # ax.loglog(df_data[1].values,df_data[2].values,"-o",color="darkgreen",lw=0.5,ms=1)
+    # ax.set_xscale('linear')
+
+    plt.plot(df_data[1].values,df_data[2].values,"-o",color="darkgreen",lw=0.5,ms=1)
+
+
+
 
     if n == 100:
         plt.xticks(np.arange(5,15.1,2))
+        plt.yticks(np.arange(0,5.1,1))
+        plt.ylim(-0.5,5)
     elif n == 1000:
         plt.xticks(np.arange(50,150.1,20))
+        plt.yticks(np.arange(0,10.1,2))
+        plt.ylim(-0.5,10)
     elif n == 10000:
         plt.xticks(np.arange(750,1250.1,100))
+        plt.yticks(np.arange(0,6.1,1))
+        plt.ylim(-0.5,6)
     elif n == 100000:
         plt.xticks(np.arange(5000,15000.1,2000))
+        plt.yticks(np.arange(0,12.1,2))
+        plt.ylim(-0.5,12)
     else:
         plt.xticks(np.arange(50000,150000.1,20000))
+        plt.yticks(np.arange(0,18.1,3))
+        plt.ylim(-0.5,18)
 
+
+    # plt.yscale("log")
+    # plt.yticks(np.arange(10**(-1),10,4))
     plt.xlabel("Number of outliers $o$")
-    plt.ylabel("$f(x^*)$ (log scale)")
-    plt.savefig(parent+"/images/log"+str(n)+".pdf",bbox_inches="tight")
+    plt.ylabel("$f(x^*)$")
+    plt.tick_params(axis='both',direction='in')
+    plt.savefig(parent+"/images/andreani_scaled"+str(n)+".pdf",bbox_inches="tight")
     # plt.show()
     plt.close()
 
 
-n = 100
+# for n in [100,1000,10000,100000,1000000]:
+#     plot_log(n)
 
-# plot_log(n)
-plot_fit(n)
+plot_fit(1000000)
+# plot_log(1000000)
