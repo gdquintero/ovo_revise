cf71506fbddb487d8ba5a5a82b36783880484076
Gustavo <gustavoquintero9105@gmail.com>
2024-09-04 16:37:53 -0500

    Updated on 09-04-2024 at 16:37

--- sources/data_andreani_scaled2.py
+++ sources/data_andreani_scaled2.py
@@ -16,11 +16,18 @@ def gen_data(m):
     noutliers = 0
 
     for i in range(m):
-        y[i] = y[i] + 0.5 * (2 * random.random() - 1)
+        y[i] = y[i] + 0.8 * (2 * random.random() - 1)
 
         if random.random() <= 0.1:
             noutliers += 1
-            y[i] = 5 + 7.5 * random.random()
+            # y[i] = 5 + 7.5 * random.random()
+            operacion = np.random.choice([0,1],p=[0.2, 0.8])
+            r = np.random.uniform(5, 10)
+
+            if operacion == 1:
+                y[i] += r
+            else:
+                y[i] -= r
 
 
     with open(parent+"/data/andreani"+str(m)+".txt","w") as f:
@@ -35,9 +42,9 @@ def gen_data(m):
 
 xsol = np.array([0,2,-3,1])
 
-# gen_data(100)
+gen_data(100)
 # gen_data(1000)
 # gen_data(10000)
-gen_data(100000)
+# gen_data(100000)
 # gen_data(1000000)
 
