24808048f14abdd81f9a2aa71b4264a9a2c1ff7a
Gustavo <gustavoquintero9105@gmail.com>
2024-09-04 19:50:29 -0500

    Updated on 09-04-2024 at 19:50

--- sources/data_andreani_scaled2.py
+++ sources/data_andreani_scaled2.py
@@ -21,13 +21,15 @@ def gen_data(m):
         if random.random() <= 0.1:
             noutliers += 1
             # y[i] = 5 + 7.5 * random.random()
+            actualizar = np.random.choice([True, False], p=[0.5, 0.5])
+
             operacion = np.random.choice([0,1],p=[0.2, 0.8])
-            r = np.random.uniform(5, 10)
+            ruido = np.random.uniform(5, 15)
 
             if operacion == 1:
-                y[i] += r
+                y[i] = ruido
             else:
-                y[i] -= r
+                y[i] = -ruido
 
 
     with open(parent+"/data/andreani"+str(m)+".txt","w") as f:
@@ -42,9 +44,9 @@ def gen_data(m):
 
 xsol = np.array([0,2,-3,1])
 
-gen_data(100)
+# gen_data(100)
 # gen_data(1000)
 # gen_data(10000)
-# gen_data(100000)
+gen_data(100000)
 # gen_data(1000000)
 
