8d24cc43994446137a08f4c62b17cc2567ec5a9e
Gustavo <gustavoquintero9105@gmail.com>
2024-09-06 17:46:36 -0500

    Updated on 09-06-2024 at 17:46

--- sources/data_andreani_scaled2.py
+++ sources/data_andreani_scaled2.py
@@ -10,20 +10,19 @@ parent =  os.path.abspath(os.path.join(cwd,os.pardir))
 def gen_data(m):
     t = np.linspace(-1,3.5,m)
     y = models.andreani(t,*xsol)
+    noutliers = 0
 
     random.seed(123456)
-
-    noutliers = 0
-    r = 0.5
+    r = 0.1
 
     for i in range(m):
         y[i] = y[i] + random.uniform(-r,r)
 
-        if random.random() <= 0.1:
+        if random.random() <= 0.2:
             noutliers += 1
 
-            operacion = np.random.choice([0,1],p=[0.3, 0.7])
-            r2 = 5 + 10 * random.random()
+            operacion = np.random.choice([0,1],p=[0.2, 0.8])
+            r2 = 12 + 2 * random.random()
 
             if operacion == 1:
                 y[i] = r2
@@ -43,12 +42,12 @@ def gen_data(m):
 
 xsol = np.array([0,2,-3,1])
 
-for n in [100,1000,10000,100000,1000000]:
-    gen_data(n)
+# for n in [100,1000,10000,100000,1000000]:
+#     gen_data(n)
 
 # gen_data(100)
 # gen_data(1000)
-# gen_data(10000)
+gen_data(10000)
 # gen_data(100000)
 # gen_data(1000000)
 
